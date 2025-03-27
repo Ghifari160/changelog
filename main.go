b2a3bc22d89d0e28308030f4469b676976332d40
GHIFARI160 <ghifari@ghifari160.com>
2025-03-27 00:09:20 -0500

    Bump version to 0.2.0
    Bump version at the beginning of the release cycle.

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	cmd := cli.App{
 		Name:                  "Changelog",
 		HelpName:              "changelog",
-		Version:               "0.1.0",
+		Version:               "0.2.0",
 		Copyright:             "(c) 2025 GHIFARI160",
 		HideVersion:           true,
 		CustomAppHelpTemplate: helpTemplate,
